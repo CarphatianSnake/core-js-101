a465559f6b0e4d49aa729db5e2d1bfcd0e93b056
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-05-26 12:39:31 +0300

    feat: implement getDigitalRoot func

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -302,8 +302,10 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) return num;
+  const sum = `${num}`.split('').reduce((acc, n) => acc + +n, 0);
+  return sum < 10 ? sum : getDigitalRoot(sum);
 }
 
 
