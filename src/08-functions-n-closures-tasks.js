46a1efc2ba50a9df279ce201f3e453d6fe30a82a
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-05-26 15:06:28 +0300

    feat: implement getIdGeneratorFunction func

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -171,8 +171,13 @@ const partialUsingArguments = (fn, ...args1) => (...args2) => fn(...args1, ...ar
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const currId = id;
+    id += 1;
+    return currId;
+  };
 }
 
 
